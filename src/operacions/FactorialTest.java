82d36fd2667b18efb3e705296b2081b0bb9ea0b6
cfgs <sergi.sg23905@gmail.com>
2024-03-18 16:42:55 +0100

    Nuevo test

--- src/operacions/FactorialTest.java
+++ src/operacions/FactorialTest.java
@@ -22,5 +22,11 @@ public class FactorialTest {
 	public void getExceptionTest() {
 		double valor = fn.factorialIteratiu(-3);
 		System.out.println(valor);
+
+	}
+
+	@Test
+	public void test2() {
+		assertEquals(120, fn.factorialRecursiu(5), 0.001);
 	}
 }
